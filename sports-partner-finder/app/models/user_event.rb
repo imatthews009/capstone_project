ccfcc2818dff126b7a44961b01446abf0f61ec87
Ian <imatthews009@gmail.com>
2017-10-18 14:43:38 -0700

    added to edit devise user page, added validation in usersports and userevents

--- sports-partner-finder/app/models/user_event.rb
+++ sports-partner-finder/app/models/user_event.rb
@@ -1,4 +1,5 @@
 class UserEvent < ApplicationRecord
+    validates_uniqueness_of :event_id, scope: :user_id
     belongs_to :user 
     belongs_to :event
 end
--- sports-partner-finder/config/routes.rb
+++ sports-partner-finder/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
   root "users#index"
 end
