9f2bfbb3b518878766ebb1ebd92f02f1656bbc0d
Ian <imatthews009@gmail.com>
2017-10-22 15:41:06 -0700

    update chats link and changed User index so current user can't see themself

--- sports-partner-finder/app/controllers/chats_controller.rb
+++ sports-partner-finder/app/controllers/chats_controller.rb
@@ -1,22 +1,22 @@
 class ChatsController < ApplicationController
     # before_action :require_login
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
       def index
         chats = current_user.chats
         @existing_chats_users = current_user.existing_chats_users
       end
       def create
-        @other_user = User.find(params[:other_user])
+        @other_user = User.find(params[:user])
         @chat = find_chat(@other_user) || Chat.new(identifier: SecureRandom.hex)
         if !@chat.persisted?
           @chat.save
           @chat.subscriptions.create(user_id: current_user.id)
           @chat.subscriptions.create(user_id: @other_user.id)
         end
-        redirect_to user_chat_path(current_user, @chat,  :other_user => @other_user.id) 
+        redirect_to user_chat_path(current_user, @chat,  :user => @other_user.id) 
       end
       def show
-        @other_user = User.find(params[:other_user])
+        @other_user = User.find(params[:user])
         @chat = Chat.find_by(id: params[:id])
         @message = Message.new
       end
@@ -35,7 +35,5 @@ class ChatsController < ApplicationController
         nil
       end
 
-      # def require_login
-      #   redirect_to new_session_path unless logged_in?
-      # end
+
 end
