f95788ff706b0963553080ce724b437f5cca2a9b
Ian <imatthews009@gmail.com>
2017-11-09 19:44:56 -0800

    added theme folder

---
+++ api-test/main.js
@@ -0,0 +1,34 @@
+console.log(User.first.username)
+$.ajax({
+  url: 'https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=5c1e467b17854b81a86c4ccc34d3eb6e',
+  dataType: 'json',
+  success: function(results) {
+    for(var i = 0; i < results['articles'].length; i++) {
+      if(results.articles[i].url.includes('mlb')) {
+        $('.sportsData').append("<h1>"+results.articles[i].url+"</h1>")
+      }
+    }
+    for(var i = 0; i < results['articles'].length; i++) {
+      if(results.articles[i].url.includes('mlb')) {
+        $('.sportsData').append("<h1>"+results.articles[i].url+"</h1>")
+      }
+    }
+    // console.log(mlbArticleArray);
+  }
+});
+
+
+
+// console.log('test');
+// $.ajax({
+//     url: 'https://newsapi.org/v1/articles',
+//     dataType: 'jsonp',
+//     data: {
+//         source: 'espn',
+//         sortBy: 'top',
+//         apiKey: '5c1e467b17854b81a86c4ccc34d3eb6e'
+//     },
+//     succes: function(results) {
+//         console.log(results);
+//     }
+// });
