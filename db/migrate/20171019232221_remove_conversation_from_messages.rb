e8c60a356c44b55565c40f856e4349f8ade456e0
Ian <imatthews009@gmail.com>
2017-12-07 21:25:57 -0800

    removed migration for heroku

--- db/migrate/20171019232221_remove_conversation_from_messages.rb
+++
@@ -1,6 +0,0 @@
-class RemoveConversationFromMessages < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :messages, :conversation_id, :integer
-    remove_column :messages, :user_id, :integer
-  end
-end
