afcf9da69fe1f97c2cecc18ce67d5e4644948fd9
Ian <imatthews009@gmail.com>
2017-12-07 21:28:09 -0800

    removed migration for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Sports.create([{ name: 'Tennis' }, { name: 'Golf' }, { name: 'Basketball', { name: 'Badminton' } }])
+Sports.create([{ name: 'Tennis' }, { name: 'Golf' }, { name: 'Basketball', { name: 'Badminton' }])
