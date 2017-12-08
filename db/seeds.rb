109237712448d869ef908ad2c3cb00c02d9d5100
Ian <imatthews009@gmail.com>
2017-12-07 21:06:43 -0800

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+sports = Sports.create([{ name: 'Tennis' }, { name: 'Golf' }, { name: 'Basketball', { name: 'Badminton' } }])
