bb220396d9a6e627e69767249101e9d1dc34accf
Ian <imatthews009@gmail.com>
2017-12-07 21:23:54 -0800

    removed migration for heroku

--- db/migrate/20171019231934_remove.rb
+++
@@ -1,7 +0,0 @@
-class Remove < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :conversations, :sender_id, :integer
-    remove_column :conversations, :recipient_id, :integer
-
-  end
-end
